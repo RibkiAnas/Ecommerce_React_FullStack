2d9e6180baf8df4fb53c6216d5b4fecd822ad26c
RibkiAnas <anasribki147@gmail.com>
2022-08-27 13:26:17 +0100

    building the project initial structure

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function App() {
-  return <div className="App">Hello this is my First Ecommerce App</div>;
+  return (
+    <div className="App">
+      <Header />
+      <h1>Hello this is my First Ecommerce App</h1>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
